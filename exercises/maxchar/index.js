4009d7ac78b4fbcd4fc969859c699cfb8f6a2809
Nick Kronberger <nickkronberger@gmail.com>
2020-09-05 16:25:26 -0400

    maxChar complete

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+    let charMap = {};
+        
+    for(let char of str){
+        if(charMap[char]){
+            charMap[char]++
+        }           
+        else{
+            charMap[char] = 1;
+        }
+    }
+
+    let highKey='';
+    let highVal=0;
+    for(let char in charMap){
+        if(charMap[char]>highVal)
+            highKey=char;
+            highVal=charMap[char];
+    }
+    return highKey;
+}
 
 module.exports = maxChar;
