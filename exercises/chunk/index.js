9a9fa30e8421022c7648dc85692617b302c6a5f9
Nick Kronberger <nickkronberger@gmail.com>
2020-09-06 16:21:06 -0400

    chunk & fizzbuzz

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,22 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+
+    //create output array
+    //loop through input array
+        //create new array[size]
+            //loop to add elements from input array to new array
+        //add new array to output array
+    let outArr=new Array; 
+
+    let chunk;
+    while(array.length>0){
+        chunk=array.splice(0,size)
+        outArr.push(chunk);
+    }
+    return outArr;
+
+}
 
 module.exports = chunk;
