f1db051474e30e2f10b9cad1a55af4870ed0eec6
Nick Kronberger <nickkronberger@gmail.com>
2020-09-16 11:06:51 -0400

    fib recursion

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -10,14 +10,24 @@
 
 function fib(n) {
 
-   const result=[0,1];
+    if (n < 2) {
+        return n;
+    }
 
-   for(let i = 2;i <= n; i++){
-       const a = result[i-1];
-       const b = result[i - 2];
-       result.push(a+b);
-   }
-   return result[n];
+    return fib(n - 1) + fib(n - 2);
+
 }
 
 module.exports = fib;
+
+// function fib(n) {
+
+//     const result=[0,1];
+
+//     for(let i = 2;i <= n; i++){
+//         const a = result[i-1];
+//         const b = result[i - 2];
+//         result.push(a+b);
+//     }
+//     return result[n];
+//  }
